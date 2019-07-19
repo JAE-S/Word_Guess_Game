8fec3401ad804497068efeadb53c9abdce26afe6
Jessie S <jessie.sholler@gmail.com>
2019-07-19 00:09:01 -0400

    sound effects added

--- assets/javascript/game_2.js
+++ assets/javascript/game_2.js
@@ -1,16 +1,15 @@
-
-// look into .split() method - takes a string and returns an array
-// Could take the selected word and split it into an array of letters,
-// var.copyarray with splice function 
-
-// Global variables 
+/*
+========================================
+Global Variables
+========================================
+*/
 var guess;                            // User guess
 var correctLetters = [];             // correctly guessed letters
 var wrongLetters = [];              // Incorrectly guessed letters
 var remainingGuesses = 5;          // Counts wrong guesses
 var losses = 0;
 var wins = 0;
-var correctWord; 
+var correctWord;                  // Randomly selected word
 let gameOver = false;
 
 var play = document.getElementById("launchedGame");  // Hides the main game page until the game is won or lost
@@ -19,7 +18,20 @@ var restartButton = document.getElementById("restart"); // Hides restart button 
 restartButton.style.display = "none";
 console.log(restart);
 
-
+/*
+========================================
+Sound Effects
+========================================
+*/
+var sound1 = document.getElementById("myAudio");
+var sound2 = document.getElementById("myAudio2");
+var sound3 = document.getElementById("myAudio3"); 
+var sound4 = document.getElementById("myAudio4");
+/*
+========================================
+Array of Planets
+========================================
+*/
 var wordList = ["MERCURY",
                 "VENUS", 
                 "EARTH", 
@@ -29,50 +41,59 @@ var wordList = ["MERCURY",
                 "URANUS", 
                 "NEPTUNE"
                 ]; 
-
-
-
-function launch(){
-    gameOver = false          // When the launch button is clicked the instructions will be hidden
+/*
+========================================
+Launch function for Intro Page
+========================================
+*/
+function launch(){  
+    gameOver = false         
     var instructions = document.getElementById("instructions");  // Selects instructions section
-    if (instructions.style.display === "none") {             
+    if (instructions.style.display === "none") {  // When the launch button is clicked the instructions will be hidden         
         instructions.style.display = "block";                 
       } else {
         instructions.style.display = "none";
       } 
-      play.style.display = "block";                
-      correctLetters = [];                                           // Resets correctly guessed letters
-      wrongLetters = [];                                            // Resets incorrectly guessed letters
+      sound1.play();
+      sound4.play();
+      play.style.display = "block";  // Shows elements in the Launched game section
       word = wordList[Math.floor(Math.random() * wordList.length)]; // Randomly selects word
       correctWord = word.split("");                                 // Converts randomly selected word from an array to a string
       for (i = 0; i < word.length; i++) {
-          correctLetters[i] = "_";                                         // Placeholder for gguessed letters 
+          correctLetters[i] = "_";                                 // Placeholder for guessed letters 
           console.log(word);
           document.getElementById("answer").innerHTML = correctLetters.join(" ");  // Joins guessed letters into a string
       }
 }
+/*
+========================================
+Restart Function for new games
+========================================
+*/
 function restart(){
-    if (gameOver === true){        // When the launch button is clicked the instructions will be hidden
-    restartButton.style.display = "none";                
+    if (gameOver === true){        // When game over is true -> reset the following:
+    restartButton.style.display = "none";                           // Hides the reset button 
     correctLetters = [];                                           // Resets correctly guessed letters
-    // document.getElementById("answer").innerHTML = (" ");
     wrongLetters = [];
-    document.getElementById("wrongGuesses").innerHTML = (" ");
+    document.getElementById("wrongGuesses").innerHTML = (" ");    // Resets incorrectly guessed letters in the html doc
     remainingGuesses = 5;     
-    document.getElementById("remainingGuesses").innerHTML = remainingGuesses; 
+    document.getElementById("remainingGuesses").innerHTML = remainingGuesses;  // Resets incorrectly guessed letters in the html doc
     }
-    gameOver = false         
+    gameOver = false 
+    sound4.play();                                                 // Alerts that a new game has started 
     word = wordList[Math.floor(Math.random() * wordList.length)]; // Randomly selects word
     correctWord = word.split("");                                 // Converts randomly selected word from an array to a string
       for (i = 0; i < word.length; i++) {
-          correctLetters[i] = "_";                                         // Placeholder for gguessed letters 
+          correctLetters[i] = "_";                               // Placeholder for guessed letters 
           console.log(word);
           document.getElementById("answer").innerHTML = correctLetters.join(" ");  // Joins guessed letters into a string
       }
     }
-
-
-// checks if guessed letter is in the correctWord
+/*
+========================================
+Check Letter function 
+========================================
+*/
 function checkLetter() {
     document.onkeyup = function(event) {
         console.log('gameOver', gameOver)
@@ -103,6 +124,8 @@ function checkLetter() {
             //Losses 
             if (remainingGuesses === 0) {
                 losses++;
+                sound4.pause();
+                sound2.play();
                 document.getElementById("losses").innerHTML = losses;
                 restartButton.style.display = "block";
                 gameOver = true
@@ -111,15 +134,15 @@ function checkLetter() {
             // Wins
             if (correctWord.toString() === correctLetters.toString()) {
                 wins++;
+                sound4.pause();
+                sound3.play();
                 document.getElementById("wins").innerHTML = wins;
                 restartButton.style.display = "block";
                 console.log(correctWord);
                 console.log(correctLetters);
                 gameOver = true
                 }
-        
         }
-        
     }
 
 checkLetter();
