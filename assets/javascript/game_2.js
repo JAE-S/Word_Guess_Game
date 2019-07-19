618762592c594e39ec6997259cbc751e107dfd64
Jessie S <jessie.sholler@gmail.com>
2019-07-18 21:03:38 -0400

    bugs fixed for reset button and wins counter

--- assets/javascript/game_2.js
+++ assets/javascript/game_2.js
@@ -2,8 +2,8 @@
 // look into .split() method - takes a string and returns an array
 // Could take the selected word and split it into an array of letters,
 // var.copyarray with splice function 
-// then 
 
+// Global variables 
 var guess;                            // User guess
 var correctLetters = [];             // correctly guessed letters
 var wrongLetters = [];              // Incorrectly guessed letters
@@ -11,10 +11,12 @@ var remainingGuesses = 5;          // Counts wrong guesses
 var losses = 0;
 var wins = 0;
 var correctWord; 
+let gameOver = false;
+
 var play = document.getElementById("launchedGame");  // Hides the main game page until the game is won or lost
 play.style.display = "none"; 
-var restart = document.getElementById("restart"); // Hides restart button until the game is won or lost
-restart.style.display = "none";
+var restartButton = document.getElementById("restart"); // Hides restart button until the game is won or lost
+restartButton.style.display = "none";
 console.log(restart);
 
 
@@ -28,14 +30,10 @@ var wordList = ["MERCURY",
                 "NEPTUNE"
                 ]; 
 
-// function restart(){   
-//     playAgain.style.disply = "none"; // Selects instructions section
-//     remainingGuesses = 5;  
-//     correctLetters = [];                           // Resets correctly guessed letters
-//     wrongLetters = []; 
-// }
 
-function launch(){          // When the launch button is clicked the instructions will be hidden
+
+function launch(){
+    gameOver = false          // When the launch button is clicked the instructions will be hidden
     var instructions = document.getElementById("instructions");  // Selects instructions section
     if (instructions.style.display === "none") {             
         instructions.style.display = "block";                 
@@ -43,7 +41,7 @@ function launch(){          // When the launch button is clicked the instruction
         instructions.style.display = "none";
       } 
       play.style.display = "block";                
-      correctLetters = [];                                                 // Resets correctly guessed letters
+      correctLetters = [];                                           // Resets correctly guessed letters
       wrongLetters = [];                                            // Resets incorrectly guessed letters
       word = wordList[Math.floor(Math.random() * wordList.length)]; // Randomly selects word
       correctWord = word.split("");                                 // Converts randomly selected word from an array to a string
@@ -53,64 +51,70 @@ function launch(){          // When the launch button is clicked the instruction
           document.getElementById("answer").innerHTML = correctLetters.join(" ");  // Joins guessed letters into a string
       }
 }
+function restart(){
+    if (gameOver === true){        // When the launch button is clicked the instructions will be hidden
+    restartButton.style.display = "block";                
+    correctLetters = [];                                           // Resets correctly guessed letters
+    wrongLetters = [];
+    remainingGuesses = 5;                                             // Resets incorrectly guessed letters
+    word = wordList[Math.floor(Math.random() * wordList.length)]; // Randomly selects word
+    correctWord = word.split("");                                 // Converts randomly selected word from an array to a string
+      for (i = 0; i < word.length; i++) {
+          correctLetters[i] = "_";                                         // Placeholder for gguessed letters 
+          console.log(word);
+          document.getElementById("answer").innerHTML = correctLetters.join(" ");  // Joins guessed letters into a string
+      }
+    }
+}
 
 // checks if guessed letter is in the correctWord
 function checkLetter() {
     document.onkeyup = function(event) {
+        console.log('gameOver', gameOver)
+        if (gameOver) {
+            return
+        }
         if (!(event.which <= 90 && event.which >= 65))  // Only letters are valid guesses
         return;
         guess = event.key.toUpperCase();                // Changes all key inputs to uppercase
-        var found = false;
             for (i = 0; i < word.length; i++) {
                 if (guess === word[i]) {
                     correctLetters[i] = guess;
                     document.getElementById("answer").innerHTML = correctLetters.join(" ");
-                    found = true;
+                    console.log(correctLetters);
                 }
             }
+            
         // Forms wrongLetters string
-        if (found) 
-        return;
-        if (wrongLetters.indexOf(guess) < 0) {
+        if (correctLetters.indexOf(guess) === -1 && wrongLetters.indexOf(guess) < 0) {
             wrongLetters.push(guess);
             document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
             remainingGuesses--;
             document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
             console.log(remainingGuesses);
         }
-        // Forms wrongLetters string
-        // if (word.indexOf(guess) != -1){                   // if the character is found
-        //     for (var i = 0; i < word.Length; i ++){        // loop on all characters
-        //     if (word[i] === guess)                        // if this is an occurance
-        //         correctWord[i] = word[i];
-
-        //     }
+    
         // }  
             //Losses 
-            // var playAgain = document.getElementById("playAgain");  // Selects instructions section
-            // if (playAgain.style.display === "none" && remainingGuesses === 0 || correctWord === correctLetters) {             
-            //     playAgain.style.display = "block";  
-            // } else {
-            //     playAgain.style.display = "block";
-            //   }               
-            
             if (remainingGuesses === 0) {
                 losses++;
-                correctLetters[i] = "_";
                 document.getElementById("losses").innerHTML = losses;
-                restart.style.display = "block";
-                // playAgain()
+                restartButton.style.display = "block";
+                gameOver = true
+                // restart();
                 }
-
+                console.log(correctWord.toString(), correctLetters.toString(), 'checking win state')
             // Wins
-            else if (correctWord === correctLetters) {
+            if (correctWord.toString() === correctLetters.toString()) {
                 wins++;
-                correctLetters[i] = "_";
                 document.getElementById("wins").innerHTML = wins;
-                restart.style.display = "block";
-                // playAgain()
+                restartButton.style.display = "block";
+                console.log(correctWord);
+                console.log(correctLetters);
+                gameOver = true
+                // restart();
                 }
-                
+        
         }
         
     }
